2ef0a55b93e7d5ccbb7a4b695c4eb58eed3ff454
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-12-06 21:23:12 +0100

    yaya

--- app/video/home/page.tsx
+++ app/video/home/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
             <header className="flex flex-col items-center justify-center text-center py-20 px-6 sm:px-16 bg-white dark:bg-zinc-900">
 
                 <h1 className="text-4xl sm:text-5xl font-bold text-black dark:text-white mb-4 max-w-3xl">
-                    StreamForge – O futuro do streaming de vídeos
+                    StreamSentra – O futuro do streaming de vídeos
                 </h1>
                 <p className="text-lg sm:text-xl text-zinc-700 dark:text-zinc-300 max-w-2xl">
                     Crie, gerencie e compartilhe vídeos de forma simples e eficiente.
-                    StreamForge combina autenticação segura, upload de vídeos rápido e processamento inteligente para transformar a sua experiência digital.
+                    StreamSentra combina autenticação segura, upload de vídeos rápido e processamento inteligente para transformar a sua experiência digital.
                 </p>
                 <div className="mt-8 flex flex-col sm:flex-row gap-4">
                     <a
